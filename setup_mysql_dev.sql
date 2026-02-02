97851632c75d08880ca47d9654843f3c40f5326f
NkemJefersonAchia <nkemachia05@gmail.com>
2026-02-02 22:30:08 +0100

    setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
 -- Creates  database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- Adds user with name  hbnb_dev identified with password 'hbnb_dev_pwd'
+-- Adds user with name  hbnb_dev identified  password 'hbnb_dev_pwd'
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- add privileges to hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
